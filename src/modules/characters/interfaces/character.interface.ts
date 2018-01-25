e0058e155e034adfc42b5054093f94aa208fa562
Arnau Lacambra <naulacambra@gmail.com>
2018-01-25 13:53:08 +0100

    Added relationship between affiliations and characters

--- src/modules/characters/interfaces/character.interface.ts
+++ src/modules/characters/interfaces/character.interface.ts
@@ -1,5 +1,8 @@
 import { Document } from 'mongoose';
+import { ObjectId } from 'bson';
 
 export interface Character extends Document {
 	readonly name: string;
+	readonly img: string;
+	readonly affiliations: ObjectId[];
 }
