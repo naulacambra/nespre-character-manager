18ee6a2fa959b9ece56f0a74c04f963c37acc80f
Arnau Lacambra <naulacambra@gmail.com>
2018-01-25 13:51:05 +0100

    Configured server to accept calls from others servers

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
+
 	app.use(bodyParser.json());
+	app.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
 	await app.listen(3000);
 }
 bootstrap();
